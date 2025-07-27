cf41d713704f83416bdf7351f530cc552f83f862
DanielAlej12 <danialej2006@gmail.com>
2025-07-27 11:42:48 -0400

    Archivos CSV y Estructuras

--- src/arbol_mago.cpp
+++ src/arbol_mago.cpp
@@ -5,6 +5,12 @@
 
 using namespace std;
 
+struct Hechizo {
+    string nombre;
+    Hechizo* siguiente;
+    Hechizo(const string& n) : nombre(n), siguiente(nullptr) {}
+};
+
 struct Mago {
     int id;
     string name;
@@ -18,10 +24,11 @@ struct Mago {
     Mago* left;  // primer discípulo
     Mago* right; // segundo discípulo
     Mago* father; // apuntador al padre para facilitar búsquedas
+    Hechizo* listaHechizos;
 
     Mago(int _id, string _name, string _last_name, char _gender, int _age, int _id_father,
          bool _is_dead, string _type_magic, bool _is_owner)
         : id(_id), name(_name), last_name(_last_name), gender(_gender), age(_age),
           id_father(_id_father), is_dead(_is_dead), type_magic(_type_magic), is_owner(_is_owner),
-          left(nullptr), right(nullptr), father(nullptr) {}
+          left(nullptr), right(nullptr), father(nullptr), listaHechizos(nullptr) {}
 };
